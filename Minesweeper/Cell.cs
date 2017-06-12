47bda8f156176c6552c59337dcfb05fdd9f595d6
Joe Martell <Joe.Martell@sjerhombus.com>
2017-06-12 12:17:34 -0700

    Minesweeper clone created after Coding Train's minesweeper challenge.

---
+++ Minesweeper/Cell.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Minesweeper {
+    class Cell {
+        public Point Location { get; }
+        public bool Mine { get; set; }
+        public bool Revealed { get; private set; }
+        public int Width { get; }
+        public void Reveal() {
+            Revealed = true;
+        }
+
+        public int Count { get; set; }
+
+        private Point TopLeft() {
+            return new Point(Location.X * Width, Location.Y * Width);
+        }
+
+        private Rectangle DrawArea() {
+            return new Rectangle(TopLeft(), new Size(Width, Width));
+        }
+
+        public Cell(Point location, int width) {
+            Location = location;
+            Width = width;
+            Mine = false;
+        }
+
+
+        public void Paint(Graphics g) {
+            if (Revealed) {
+                if (Mine) {
+                    g.FillRectangle(Brushes.Red, DrawArea());
+                }
+                else {
+                    if (Count > 0) {
+                        g.DrawString(this.Count.ToString(), new Font("Arial", 12), Brushes.Black, DrawArea());
+                    }
+                }
+            }
+            else {
+                g.FillRectangle(Brushes.Gray, DrawArea());
+            }
+            g.DrawRectangle(Pens.Black, DrawArea());
+        }
+
+        public bool Contains(Point clickSpot) {
+            return DrawArea().Contains(clickSpot);
+        }
+
+        public IEnumerable<Point> Neighbors(int colCount, int rowCount) {
+            List<Point> neighbors = new List<Point>();
+            for (int i = -1; i < 2; i++) {
+                for (int j = -1; j < 2; j++) {
+                    int newI = Location.X + i;
+                    int newJ = Location.Y + j;
+
+                    if ((newI >= 0) && (newI < colCount) && (newJ >= 0) && (newJ < rowCount)) {
+                        neighbors.Add(new Point(newI, newJ));
+                    }
+                }
+
+            }
+
+            return neighbors;
+        }
+    }
+}
---
+++ Minesweeper/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper {
+    public partial class Form1 : Form {
+        private int rowCount = 20;
+        private int colCount = 20;
+        private int width = 20;
+        private int mineCount = 40;
+        private List<Cell> cells;
+
+        private Cell[,] gameBoard;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            SetBoard();
+        }
+
+        private void SetBoard() {
+            cells = new List<Cell>();
+            Cell[] test = new Cell[100];
+            gameBoard = new Cell[colCount, rowCount];
+            for (int i = 0; i < colCount; i++) {
+                for (int j = 0; j < rowCount; j++) {
+                    gameBoard[i, j] = new Cell(new Point(i, j), width);
+                    cells.Add(gameBoard[i, j]);
+                }
+            }
+
+            cells.Shuffle();
+            var MineCells = cells.Take(mineCount);
+            foreach (var c in MineCells) {
+                c.Mine = true;
+            }
+            SetCellCounts();
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e) {
+            for (int i = 0; i < colCount; i++) {
+                for (int j = 0; j < rowCount; j++) {
+                    gameBoard[i, j].Paint(e.Graphics);
+                }
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e) {
+            for (int i = 0; i < colCount; i++) {
+                for (int j = 0; j < rowCount; j++) {
+                    if (gameBoard[i, j].Contains(e.Location)) {
+                        if (!gameBoard[i,j].Revealed) {
+                            gameBoard[i, j].Reveal();
+                            if (gameBoard[i, j].Count == 0) {
+                                CascadeReveal(gameBoard[i, j].Neighbors(colCount, rowCount));
+                            }
+                            if (gameBoard[i,j].Mine) {
+                                RevealBoard();
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+            this.Invalidate();
+        }
+
+        private void SetCellCounts() {
+            for (int i = 0; i < colCount; i++) {
+                for (int j = 0; j < rowCount; j++) {
+                    for (int iMod = -1; iMod <= 1; iMod++) {
+                        for (int jMod = -1; jMod <= 1; jMod++) {
+                            int colIndex = i + iMod;
+                            int rowIndex = j + jMod;
+                            if ((colIndex >= 0) && (colIndex < colCount) &&
+                                (rowIndex >= 0) && (rowIndex < rowCount)) {
+                                if (gameBoard[colIndex, rowIndex].Mine) {
+                                    gameBoard[i, j].Count++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void CascadeReveal(IEnumerable<Point> cells) {
+            foreach (var c in cells) {
+                Cell targetCell = gameBoard[c.X, c.Y];
+                if (!targetCell.Mine && !targetCell.Revealed) {
+                    targetCell.Reveal();
+                    if (targetCell.Count <= 0) {
+                        CascadeReveal(targetCell.Neighbors(colCount, rowCount));
+                    }
+                }
+            }
+        }
+
+
+        private void RevealBoard() {
+            for (int i = 0; i < colCount; i++) {
+                for (int j = 0; j < rowCount; j++) {
+                    gameBoard[i, j].Reveal();
+                }
+            }
+        }
+
+        private void btnResetBoard_Click(object sender, EventArgs e) {
+            SetBoard();
+            Invalidate();
+        }
+    }
+}
